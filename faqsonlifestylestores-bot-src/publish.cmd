4895158c4afad6142848eb95629737bf79cd8b9e
Mohammed Kammur <111441054+Kammurmohammed@users.noreply.github.com>
2022-08-30 15:49:50 +0530

    Add files via upload

---
+++ faqsonlifestylestores-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\faqsonlifestylestores-bot-986c-Web-Deploy.pubxml -p:Password=EJoXa0nw5CmwQCddZf1vdZmLmwv82p7geZbkgfqZrxvrYMq6qZmePBhspJu4
+
